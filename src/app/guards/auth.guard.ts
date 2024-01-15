1a7828ba97817e178d02b8c3e4ed03d6ac374d99
Jorge Monteiro <jorgemonteiro@ses.mt.gov.br>
2024-01-15 16:38:38 -0400

    Fix: delete lógico e autenticação

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,12 +1,33 @@
 import { inject } from '@angular/core';
 import { CanActivateChildFn, Router } from '@angular/router';
+import { AccountService } from '../services/account.service';
+import { Observable } from 'rxjs';
+import { SharedService } from '../services/shared.service';
 
 export const authGuard: CanActivateChildFn = (childRoute, state) => {
   const router = inject(Router);
+  const user = inject(AccountService).getCredentials()
+  const sharedService = inject(SharedService)
   const token = window.localStorage.getItem('token')
-  if (token) {
-    return true;
-  }
-  router.navigate(['/login'])
-  return false;
+
+  return new Observable<boolean>(obs => {
+    user.subscribe({
+      next: (response: any) => {
+        if (response.is_valid && token) {
+          obs.next(true);
+        } else {
+          obs.next(false);
+          window.localStorage.clear();
+          sharedService.showMessage('Credenciais inválida')
+          router.navigate(['/login'])
+        }
+      },
+      error: () => {
+        obs.next(false);
+        window.localStorage.clear();
+        router.navigate(['/login'])
+      }
+    })
+  });
+
 };
